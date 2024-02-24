2154056f31c1ad5425c3cc9ebc54e4615546f420
Sobirov3636 <sobirov3636@gmail.com>
2024-02-24 22:06:43 +0900

    feat: connect mongoDB via mongoose

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,12 @@
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
+mongoose
+  .connect(process.env.MONGO_URL as string, {})
+
+  .then((data) => {
+    console.log("MongoDB connection succeed");
+    const PORT = process.env.PORT ?? 3003;
+  })
+  .catch((err) => console.log("ERROR on connection mongoDB", err));
