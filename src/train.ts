8512fe09d7bce4b260b3351e5be0ffc416cf8cb8
Sobirov3636 <sobirov3636@gmail.com>
2024-02-25 23:36:15 +0900

    feat: sezer challenge solution

--- src/train.ts
+++ src/train.ts
@@ -1,1 +1,21 @@
-console.log("Hello World!");
+// Sezer challenge
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
+let code = 2;
+let message = "hello, how are you doing, my general?";
+const decodeMessage = (message: string, secret: number) => {
+  const result = message.replace(/[a-z]/gi, (char) => {
+    const index = alphabet.indexOf(char.toLowerCase());
+    const newIndex = (index + secret + alphabet.length) % alphabet.length;
+    const decodedChar = alphabet[newIndex];
+    return decodedChar;
+  });
+  return result;
+};
+const secret_msg = decodeMessage(message, code);
+console.log("Secret Message =>", secret_msg);
+
+setTimeout(() => {
+  code *= -1;
+  const message_encoded = decodeMessage(secret_msg, code);
+  console.log("Original Message => ", message_encoded);
+}, 5000);
