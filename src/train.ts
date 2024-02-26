1211142edd99e1fa35ed7ecf8d1568edbdf2604e
Sobirov3636 <sobirov3636@gmail.com>
2024-02-26 23:29:24 +0900

    feat: H-TASK solution

--- src/train.ts
+++ src/train.ts
@@ -1,21 +1,35 @@
-// Sezer challenge
-const alphabet = "abcdefghijklmnopqrstuvwxyz";
-let code = 2;
-let message = "hello, how are you doing, my general?";
-const decodeMessage = (message: string, secret: number) => {
-  const result = message.replace(/[a-z]/gi, (char) => {
-    const index = alphabet.indexOf(char.toLowerCase());
-    const newIndex = (index + secret + alphabet.length) % alphabet.length;
-    const decodedChar = alphabet[newIndex];
-    return decodedChar;
-  });
-  return result;
+// H-TASK
+const getPositive = (arr: number[]) => {
+  let res: string = "";
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] >= 0) {
+      res += arr[i];
+    }
+  }
+  return res;
 };
-const secret_msg = decodeMessage(message, code);
-console.log("Secret Message =>", secret_msg);
 
-setTimeout(() => {
-  code *= -1;
-  const message_encoded = decodeMessage(secret_msg, code);
-  console.log("Original Message => ", message_encoded);
-}, 5000);
+const result = getPositive([1, 2, 3, 4, 0, -1, -4, 6, -8]);
+console.log(result);
+
+// // Sezer challenge
+// const alphabet = "abcdefghijklmnopqrstuvwxyz";
+// let code = 2;
+// let message = "hello, how are you doing, my general?";
+// const decodeMessage = (message: string, secret: number) => {
+//   const result = message.replace(/[a-z]/gi, (char) => {
+//     const index = alphabet.indexOf(char.toLowerCase());
+//     const newIndex = (index + secret + alphabet.length) % alphabet.length;
+//     const decodedChar = alphabet[newIndex];
+//     return decodedChar;
+//   });
+//   return result;
+// };
+// const secret_msg = decodeMessage(message, code);
+// console.log("Secret Message =>", secret_msg);
+
+// setTimeout(() => {
+//   code *= -1;
+//   const message_encoded = decodeMessage(secret_msg, code);
+//   console.log("Original Message => ", message_encoded);
+// }, 5000);
