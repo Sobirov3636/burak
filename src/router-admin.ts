6f3aa0acad259220a3c76931b5cbfd96290b6f35
Sobirov3636 <sobirov3636@gmail.com>
2024-03-09 16:43:06 +0900

    feat: create checkAuthSession api

--- src/router-admin.ts
+++ src/router-admin.ts
@@ -6,6 +6,7 @@ import restaurantController from "./controllers/restaurant.controller";
 routerAdmin.get("/", restaurantController.goHome);
 routerAdmin.get("/login", restaurantController.getLogin).post("/login", restaurantController.processLogin);
 routerAdmin.get("/signup", restaurantController.getSignup).post("/signup", restaurantController.processSignup);
+routerAdmin.get("/check-me", restaurantController.checkAuthSession);
 
 /** Product*/
 /** User*/
