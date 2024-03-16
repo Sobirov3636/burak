9df4cb4182b99338f3d002a66766b4c0298e368d
Sobirov3636 <sobirov3636@gmail.com>
2024-03-16 16:19:38 +0900

    fix: finish frontend restaurant signup process

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,6 +1,27 @@
 console.log("Signup frontend javascript file");
 
-$(function () {});
+$(function () {
+  const fileTarget = $(".file-box .upload-hidden");
+  let fileName;
+  fileTarget.on("change", function () {
+    if (window.FileReader) {
+      const uploadFile = $(this)[0].files[0];
+      console.log("uploadFile => ", uploadFile);
+      const fileType = uploadFile["type"];
+      const validImageType = ["image/jpg", "image/jpeg", "image/png"];
+      if (!validImageType.includes(fileType)) {
+        alert("Please insert only jpeg, jpg and png!");
+      } else {
+        if (uploadFile) {
+          console.log(URL.createObjectURL(uploadFile));
+          $(".upload-img-frame").attr("src", URL.createObjectURL(uploadFile)).addClass("success");
+        }
+        fileName = $(this)[0].files[0].name;
+      }
+      $(this).siblings(".upload-name").val(fileName);
+    }
+  });
+});
 
 function validateSignupForm() {
   const memberNick = $(".member-nick").val();
@@ -14,6 +35,13 @@ function validateSignupForm() {
 
   if (memberPassword !== confirmPassword) {
     alert("Password differs, please check!");
+    return false;
+  }
+
+  const memberImage = $(".member-image").get(0).files[0] ? $(".member-image").get(0).files[0].name : null;
+
+  if (!memberImage) {
+    alert("Please insert restaurant image");
     return false;
   }
 }
